5d364057b7c58490aef0d8135ac93a8688370074
KlausWastu <20k10036@student.unika.ac.id>
2023-07-04 13:35:40 +0700

    mendaftarkan router dashboard dan departemen

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var dashboardRouter = require('./app/dashboard/router');
+var departemenRouter = require('./app/departemen/router');
 
 var app = express();
 
@@ -13,14 +13,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use("/adminlte", express.static(path.join(__dirname, "/node_modules/admin-lte/")))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', dashboardRouter);
+app.use('/departemen', departemenRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
