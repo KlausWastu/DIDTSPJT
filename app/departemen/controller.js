df7d6a3e88021ac27972ccd462798284c9caf742
KlausWastu <20k10036@student.unika.ac.id>
2023-07-04 16:14:47 +0700

    Membuat logic dan routing untuk menampilkan halaman create departemen

--- app/departemen/controller.js
+++ app/departemen/controller.js
@@ -1,11 +1,24 @@
+const Departemen = require("./model")
 module.exports = {
     index: async(req,res)=> {
+        const departemen = await Departemen.find({isdeleted: false})
         try {
             res.render("admin/departemen/view_departemen", {
-                title: "Departemen"
+                title: "Departemen",
+                departemen
             })
-        } catch (error) {
-            console.log(error)
+        } catch (err) {
+            console.log(err)
         }
-    }
+    }, 
+    viewCreate:async(req,res)=>{
+        try {
+            res.render("admin/departemen/create",{
+                title: "Tambah Departemen"
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }, 
+    
 }
