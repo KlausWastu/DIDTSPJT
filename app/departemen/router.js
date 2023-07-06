a386f8ad02a11eb6d879ed9459ec3354c048c90a
KlausWastu <20k10036@student.unika.ac.id>
2023-07-06 16:29:54 +0700

    Membuat router untuk menampilkan halaman edit departemen

--- app/departemen/router.js
+++ app/departemen/router.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const {index, viewCreate, createDepartemen} = require("./controller")
+const {index, viewCreate, createDepartemen, viewEdit} = require("./controller")
 
 /* GET home page. */
 router.get('/', index);
 router.get("/create", viewCreate)
 router.post("/create",createDepartemen)
+router.get("/edit/:id", viewEdit)
 module.exports = router;
