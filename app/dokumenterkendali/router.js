58bfee51c65e1331c613b84018a13a94410e13da
KlausWastu <20k10036@student.unika.ac.id>
2023-07-10 13:59:06 +0700

    Membuat router untuk action edit dokumen

--- app/dokumenterkendali/router.js
+++ app/dokumenterkendali/router.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const {index, viewCreate, createDokumen, viewEdit} = require("./controller")
+const {index, viewCreate, createDokumen, viewEdit, editDokumen} = require("./controller");
 
 /* GET home page. */
 router.get('/', index);
 router.get("/create", viewCreate)
 router.post("/create",createDokumen)
 router.get("/edit/:id", viewEdit)
+router.put("/edit/:id", editDokumen)
 module.exports = router;
