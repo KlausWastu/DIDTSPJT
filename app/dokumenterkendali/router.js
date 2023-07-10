8b707a7cc7aab347713394991cebc7abdaaba212
KlausWastu <20k10036@student.unika.ac.id>
2023-07-10 13:43:45 +0700

    Membuat router untuk halaman edit dokumen terkendali

--- app/dokumenterkendali/router.js
+++ app/dokumenterkendali/router.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const {index, viewCreate, createDokumen} = require("./controller")
+const {index, viewCreate, createDokumen, viewEdit} = require("./controller")
 
 /* GET home page. */
 router.get('/', index);
 router.get("/create", viewCreate)
 router.post("/create",createDokumen)
+router.get("/edit/:id", viewEdit)
 module.exports = router;
