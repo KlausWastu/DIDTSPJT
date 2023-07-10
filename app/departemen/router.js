d37037de624ecd8a2ead5315b89be8242f383fce
KlausWastu <20k10036@student.unika.ac.id>
2023-07-10 11:48:40 +0700

    Menambahkan router untuk edit departemen

--- app/departemen/router.js
+++ app/departemen/router.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const {index, viewCreate, createDepartemen, viewEdit} = require("./controller")
+const {index, viewCreate, createDepartemen, viewEdit, editDepartemen} = require("./controller")
 
 /* GET home page. */
 router.get('/', index);
 router.get("/create", viewCreate)
 router.post("/create",createDepartemen)
 router.get("/edit/:id", viewEdit)
+router.put("/edit/:id", editDepartemen)
 module.exports = router;
