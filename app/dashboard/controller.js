917c1f0f578aae9fec39783bb51042662c4dc99d
KlausWastu <20k10036@student.unika.ac.id>
2023-07-12 10:53:33 +0700

    Membuat fungsi pemanggilan halaman dashboard

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,11 +1,11 @@
 module.exports = {
-    index: async(req,res)=> {
-        try {
-            res.render("admin/dashboard/dashboard", {
-                title: "Dashboard"
-            })
-        } catch (error) {
-            console.log(error)
-        }
+  index: async (req, res) => {
+    try {
+      res.render("admin/dashboard/dashboard", {
+        title: "Dashboard",
+      });
+    } catch (error) {
+      console.log(error);
     }
-}
+  },
+};
