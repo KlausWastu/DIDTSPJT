bb44978bd102104fd4109ea5e4eb376370b3d2b6
KlausWastu <20k10036@student.unika.ac.id>
2023-07-13 15:04:17 +0700

    Menambahkan router untuk logout pada router signin

--- app/user/router.js
+++ app/user/router.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const { viewSignin, actionSignin } = require("./controller");
+const { viewSignin, actionSignin, logout } = require("./controller");
 
 /* GET home page. */
 router.get("/", viewSignin);
 router.post("/", actionSignin);
+router.get("/logout", logout);
 module.exports = router;
