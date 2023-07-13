0e1afed85356fb92c1a8016d0aaa513fa4475a06
KlausWastu <20k10036@student.unika.ac.id>
2023-07-13 16:19:17 +0700

    Membuat router untuk tambah pengguna di router pengguna

--- app/pengguna/router.js
+++ app/pengguna/router.js
@@ -7,6 +7,7 @@ const {
   viewEdit,
   editDokumen,
   deleteDokumen,
+  editPengguna,
 } = require("./controller");
 const { isLogin } = require("../middleware/auth");
 
@@ -15,7 +16,7 @@ router.use(isLogin);
 router.get("/", index);
 router.get("/create", viewCreate);
 router.post("/create", createDokumen);
-// router.get("/edit/:id", viewEdit);
-// router.put("/edit/:id", editDokumen);
+router.get("/edit/:id", viewEdit);
+router.put("/edit/:id", editPengguna);
 // router.delete("/delete/:id", deleteDokumen);
 module.exports = router;
