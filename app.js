d8a7384e115ef6265f1e3317772c8a734131ab0f
KlausWastu <20k10036@student.unika.ac.id>
2023-07-13 08:32:57 +0700

    Mendaftarkan router signin di app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const flash = require("connect-flash") 
-const session = require("express-session")
-const override = require("method-override")
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const flash = require("connect-flash");
+const session = require("express-session");
+const override = require("method-override");
 
-var dashboardRouter = require('./app/dashboard/router');
-var departemenRouter = require('./app/departemen/router');
-var dokumenRouter = require('./app/dokumenterkendali/router');
+const loginRouter = require("./app/user/router");
+
+var dashboardRouter = require("./app/dashboard/router");
+var departemenRouter = require("./app/departemen/router");
+var dokumenRouter = require("./app/dokumenterkendali/router");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(
   session({
@@ -25,33 +27,37 @@ app.use(
     cookie: {},
   })
 );
-app.use("/adminlte", express.static(path.join(__dirname, "/node_modules/admin-lte/")))
-app.use(override("_method"))
-app.use(flash())
-app.use(logger('dev'));
+app.use(
+  "/adminlte",
+  express.static(path.join(__dirname, "/node_modules/admin-lte/"))
+);
+app.use(override("_method"));
+app.use(flash());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', dashboardRouter);
-app.use('/departemen', departemenRouter);
-app.use('/dokumen', dokumenRouter);
+app.use("/", loginRouter);
+app.use("/dashboard", dashboardRouter);
+app.use("/departemen", departemenRouter);
+app.use("/dokumen", dokumenRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
