3bb3297e77078f10ad95dba717168e4a90688a43
KlausWastu <20k10036@student.unika.ac.id>
2023-07-13 08:32:41 +0700

    Membuat logika untuk menampilkan halaman sigin dan fungsi signin

---
+++ app/user/controller.js
@@ -0,0 +1,65 @@
+const User = require("./model");
+const bcrypt = require("bcryptjs");
+module.exports = {
+  viewSignin: async (req, res) => {
+    try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+
+      const alert = { message: alertMessage, status: alertStatus };
+      res.render("admin/signin/view_signin", {
+        title: "Sign In",
+        alert,
+      });
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/");
+    }
+  },
+  actionSignin: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const check = await User.findOne({ email: email });
+      if (check) {
+        if (check.isdeleted === false) {
+          const checkpass = await bcrypt.compare(password, check.password);
+          if (checkpass) {
+            req.session.user = {
+              id: check._id,
+              email: check.email,
+              status: check.status,
+              role: check.role,
+              name: check.name,
+            };
+            if (check.role === "admin" || check.role === "user") {
+              res.redirect("/dashboard");
+            }
+          } else {
+            req.flash("alertMessage", `Password yang anda masukan salah`);
+            req.flash("alertStatus", "danger");
+            res.redirect("/");
+          }
+        } else {
+          req.flash(
+            "alertMessage",
+            `Akun anda sudah di non-aktifkan oleh admin, silahkan hubungi Admin dan coba lagi`
+          );
+          req.flash("alertStatus", "danger");
+          res.redirect("/");
+        }
+      } else {
+        req.flash(
+          "alertMessage",
+          `Email yang anda masukan salah, Silahkan coba lagi`
+        );
+        req.flash("alertStatus", "danger");
+        res.redirect("/");
+      }
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/");
+    }
+  },
+};
