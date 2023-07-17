ad6ca0d7a7cf204df3dde64a0bcf453ab282e314
KlausWastu <20k10036@student.unika.ac.id>
2023-07-17 11:43:04 +0700

    Membuat router untuk change password

--- app/user/router.js
+++ app/user/router.js
@@ -1,9 +1,17 @@
 var express = require("express");
 var router = express.Router();
-const { viewSignin, actionSignin, logout } = require("./controller");
+const {
+  viewSignin,
+  actionSignin,
+  logout,
+  viewubahPass,
+  ubahPass,
+} = require("./controller");
 
 /* GET home page. */
 router.get("/", viewSignin);
 router.post("/", actionSignin);
+router.get("/ubahpass", viewubahPass);
+router.put("/ubahPass/:id", ubahPass);
 router.get("/logout", logout);
 module.exports = router;
